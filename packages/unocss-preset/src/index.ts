8bcc25d655c6356130f181af09de5fc998fc7291
mfranken <miguel.franken@denkwerk.com>
2023-03-27 16:40:37 +0200

    Add static margin rule

--- packages/unocss-preset/src/index.ts
+++ packages/unocss-preset/src/index.ts
@@ -1,3 +1,12 @@
-export default function echo(input: string): string {
-  return input
+import {Preset, Rule} from "@unocss/core";
+
+const rules: Rule[] = [
+  ['m-16', { margin: '4rem' }]
+]
+
+export const presetMini = (): Preset => {
+  return {
+    name: '@denkwerk/unocss-preset',
+    rules,
+  }
 }
