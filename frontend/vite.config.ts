c9cd22df66b82810d5de0fa608266d95f59bc182
mfranken <miguel.franken@denkwerk.com>
2023-03-27 15:27:07 +0200

    Add UnoCSS npm package to frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    UnoCSS({
+      presets: []
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
