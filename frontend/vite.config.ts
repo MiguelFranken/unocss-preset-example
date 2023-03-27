467b79238c207b54e9d8c60f06a9bcea6e801a93
mfranken <miguel.franken@denkwerk.com>
2023-03-27 16:44:33 +0200

    Apply unocss preset to frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
+import { presetMini } from "@denkwerk/unocss-preset"
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     UnoCSS({
-      presets: []
+      presets: [
+        presetMini()
+      ]
     })
   ],
   resolve: {
