b008153805112070448a55051427d47d74c4bb22
mfranken <miguel.franken@denkwerk.com>
2023-04-03 11:50:55 +0200

    Internationalize frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+</script>
+
 <template>
-  <div>Hello World</div>
+  <div>{{ t('container.greeting') }}</div>
 </template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createI18n } from 'vue-i18n'
 
-createApp(App).mount('#app')
+import messages from '@intlify/unplugin-vue-i18n/messages'
+
+const i18n = createI18n({
+  locale: 'en',
+  messages
+})
+
+createApp(App).use(i18n).mount('#app')
