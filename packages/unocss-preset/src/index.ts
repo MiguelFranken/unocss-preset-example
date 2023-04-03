96d2e97cf612b5ff23cba3353f4e9dde10ca67bd
mfranken <miguel.franken@denkwerk.com>
2023-04-03 18:04:28 +0200

    Add CSS utilities

--- packages/unocss-preset/src/index.ts
+++ packages/unocss-preset/src/index.ts
@@ -1,6 +1,9 @@
 import {Preset, Rule} from "@unocss/core";
 
-const rules: Rule[] = []
+const rules: Rule[] = [
+  [/^m-(\d+)$/, ([, d]) => ({ margin: `${+d / 4}rem` })],
+  [/^p-(\d+)$/, ([, d]) => ({ padding: `${+d / 4}rem` })],
+]
 
 export const presetMini = (): Preset => {
   return {
